65b9decaea02c9a03eff07d14dd313a547acbf2a
alishergiyasov100-boop <musornibak228@gmail.com>
2026-06-14 13:57:34 +0500

    v0.2.9: Sakurada Fumiriya + idle pool (Relax/LookAround/Thinking/Sleepy random)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.korvus.pocketmiku"
         minSdk = 26
         targetSdk = 34
-        versionCode = 21
-        versionName = "0.2.8"
+        versionCode = 22
+        versionName = "0.2.9"
     }
 
     buildTypes {
