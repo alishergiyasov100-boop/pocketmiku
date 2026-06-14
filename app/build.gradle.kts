70bcee7b2442359220ee894760dd6c0dc64c8906
alishergiyasov100-boop <musornibak228@gmail.com>
2026-06-14 13:18:33 +0500

    v0.2.6: accumulate showErr lines (was overwriting first VRMA error)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.korvus.pocketmiku"
         minSdk = 26
         targetSdk = 34
-        versionCode = 18
-        versionName = "0.2.5"
+        versionCode = 19
+        versionName = "0.2.6"
     }
 
     buildTypes {
